1a0ab92f73663c365bbc228fbada5ca7006afbca
debugger999 <debugger999@163.com>
2021-10-29 15:56:20 +0800

    yolov3,run with video is ok, python code

--- yolov3/build.py
+++ yolov3/build.py
@@ -13,7 +13,7 @@ from tvm.relay.testing.darknet import __darknetffi__
 import matplotlib.pyplot as plt
 import cv2
 
-target = "llvm"
+target = "cuda"
 
 if len(sys.argv) < 5:
   print("usage: python3 build.py weights cfg libdarknet img")
@@ -22,6 +22,7 @@ weights_path = sys.argv[1]
 cfg_path = sys.argv[2]
 lib_darknet = sys.argv[3]
 img_path = sys.argv[4]
+video_path = sys.argv[4]
 
 c = 3
 h = 416
@@ -53,65 +54,68 @@ dev = tvm.device(str(target), 0)
 module = graph_executor.GraphModule(loaded_lib["default"](dev))
 print("run ok")
 
-print("resize", img_path, "to", w, "x", h, ", channel:", c)
-_shape,img_data = tvm.relay.testing.darknet.load_image(img_path, w, h)
-img_data = tvm.nd.array(img_data.astype(dtype))
-print("resize ok")
-module.set_input(input_name, img_data)
-print("graph run ...")
-module.run()
-print("run ok")
-
-tvm_out = []
-for i in range(3):
-    layer_out = {}
-    layer_out["type"] = "Yolo"
-    # Get the yolo layer attributes (n, out_c, out_h, out_w, classes, total)
-    layer_attr =module.get_output(i * 4 + 3).numpy()
-    layer_out["biases"] =module.get_output(i * 4 + 2).numpy()
-    layer_out["mask"] =module.get_output(i * 4 + 1).numpy()
-    out_shape = (layer_attr[0], layer_attr[1] // layer_attr[0], layer_attr[2], layer_attr[3])
-    layer_out["output"] =module.get_output(i * 4).numpy().reshape(out_shape)
-    layer_out["classes"] = layer_attr[4]
-    tvm_out.append(layer_out)
-#print(len(tvm_out), tvm_out)
-
-thresh = 0.5
-nms_thresh = 0.45
-classes = 80
-# do the detection and bring up the bounding boxes
-im_h, im_w, _ = _shape
-dets = tvm.relay.testing.yolo_detection.fill_network_boxes(
-    (w, h), (im_w, im_h), thresh, 1, tvm_out
-)
-tvm.relay.testing.yolo_detection.do_nms_sort(dets, classes, nms_thresh)
-
-coco_path = "coco.names"
-with open(coco_path) as f:
-    content = f.readlines()
-names = [x.strip() for x in content]
-for det in dets:
-    valid, detection = tvm.relay.testing.yolo_detection.get_detections_ex(_shape, det, thresh, names, classes)
-    if valid:
-        print(
-            "class:{} score:{} left:{} top:{} right:{} bottom:{}".format(
-                detection["classname"],
-                detection["score"],
-                detection["left"],
-                detection["top"],
-                detection["right"],
-                detection["bot"],
-            )
-        )
-
-'''
-font_path = "arial.ttf"
-img = tvm.relay.testing.darknet.load_image_color(img_path)
-tvm.relay.testing.yolo_detection.show_detections(img, dets, thresh, names, classes)
-tvm.relay.testing.yolo_detection.draw_detections(
-    font_path, img, dets, thresh, names, classes
-)
-plt.imshow(img.transpose(1, 2, 0))
-plt.show()
-'''
+cnt = 0
+cap = cv2.VideoCapture(video_path)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print("resize", img_path, int(width), "x", int(height), "to", w, "x", h, ", channel:", c)
+while True:
+  ret_val, img = cap.read()
+  if not ret_val:
+      continue
+  img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img_data = tvm.relay.testing.darknet.load_image_ex(img_rgb, w, h)
+  img_data = tvm.nd.array(img_data.astype(dtype))
+  module.set_input(input_name, img_data)
+  module.run()
+  tvm_out = []
+  for i in range(3):
+      layer_out = {}
+      layer_out["type"] = "Yolo"
+      # Get the yolo layer attributes (n, out_c, out_h, out_w, classes, total)
+      layer_attr =module.get_output(i * 4 + 3).numpy()
+      layer_out["biases"] =module.get_output(i * 4 + 2).numpy()
+      layer_out["mask"] =module.get_output(i * 4 + 1).numpy()
+      out_shape = (layer_attr[0], layer_attr[1] // layer_attr[0], layer_attr[2], layer_attr[3])
+      layer_out["output"] =module.get_output(i * 4).numpy().reshape(out_shape)
+      layer_out["classes"] = layer_attr[4]
+      tvm_out.append(layer_out)
+  thresh = 0.5
+  nms_thresh = 0.45
+  classes = 80
+  # do the detection and bring up the bounding boxes
+  im_h, im_w, _ = img_rgb.shape
+  dets = tvm.relay.testing.yolo_detection.fill_network_boxes(
+      (w, h), (im_w, im_h), thresh, 1, tvm_out
+  )
+  tvm.relay.testing.yolo_detection.do_nms_sort(dets, classes, nms_thresh)
+  coco_path = "coco.names"
+  with open(coco_path) as f:
+      content = f.readlines()
+  names = [x.strip() for x in content]
+  print("##", cnt)
+  for det in dets:
+      valid, detection = tvm.relay.testing.yolo_detection.get_detections_ex(img_rgb.shape, det, thresh, names, classes)
+      if valid:
+          classname = detection["classname"]
+          score = detection["score"]
+          x1 = detection["left"]
+          y1 = detection["top"]
+          x2 = detection["right"]
+          y2 = detection["bot"]
+          print(classname, score, x1, y1, x2, y2)
+          cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+  cv2.imshow("test", img)
+  cv2.waitKey(10)
+  '''
+  font_path = "arial.ttf"
+  img = tvm.relay.testing.darknet.convert_image(img)
+  tvm.relay.testing.yolo_detection.show_detections(img, dets, thresh, names, classes)
+  tvm.relay.testing.yolo_detection.draw_detections(
+      font_path, img, dets, thresh, names, classes
+  )
+  plt.imshow(img.transpose(1, 2, 0))
+  plt.show()
+  '''
+  cnt += 1
 
